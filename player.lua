a8327618f60823110c6df6dc3ffda5432eb301f4
Guysus <137240103+Guysus@users.noreply.github.com>
2023-12-22 13:19:04 -0800

    got image to work

--- player.lua
+++ player.lua
@@ -6,15 +6,15 @@ function Player:new(x, y)
 end
 
 function Player:update(dt) 
-  
     Player.super.update(self, dt)
     
-    if love.keypressed.isDown("left") then
+    if love.keyboard.isDown("left") then
         self.x = self.x - 200 * dt
-    elseif love.keypressed.isDown("right") then
+    elseif love.keyboard.isDown("right") then
         self.x = self.x + 200 * dt
-    elseif love.keypressed.isDown("up") then
+    elseif love.keyboard.isDown("up") then
         self.y = self.y - 200 * dt
-    elseif love.keypressed.isDown("down") then
-      self.y = self.y + 200 * dt      
+    elseif love.keyboard.isDown("down") then
+      self.y = self.y + 200 * dt 
+    end
 end
