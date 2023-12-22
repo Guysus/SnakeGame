cd6c40855ed853d7af5a823f668fab550dd4cb8f
Guysus <137240103+Guysus@users.noreply.github.com>
2023-12-22 12:20:58 -0800

    added movement in player

--- player.lua
+++ player.lua
@@ -5,9 +5,15 @@ function Player:new(x, y)
     
 end
 
-function Player:update(dt)
-  if love.keypressed.isDown("left")
-      self.x =
+function Player:update(dt) 
+  if love.keypressed.isDown("left") then
+      self.x = self.x - 200 * dt
+  elseif love.keypressed.isDown("right") then
+      self.x = self.x + 200 * dt
+  elseif love.keypressed.isDown("up") then
+      self.y = self.y - 200 * dt
+  elseif love.keypressed.isDown("down") then
+      self.y = self.y + 200 * dt
 end
 
 function Player:draw()
