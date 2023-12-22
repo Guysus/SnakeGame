570530a799d8f1c94cb4cd377d19f6de6c79d478
Guysus <137240103+Guysus@users.noreply.github.com>
2023-12-22 15:58:31 -0800

    added apple

---
+++ apple.lua
@@ -0,0 +1,7 @@
+
+Apple = Entity:extend()
+
+function Apple:new()
+    Apple.super.new(self, x, y, "Image/apple")
+end
+
--- entity.lua
+++ entity.lua
@@ -5,8 +5,8 @@ function Entity:new(x, y, image_path)
   self.x = x
   self.y = y
   self.image = love.graphics.newImage(image_path)
-  --self.width = self.image.getWidth()
-  --self.height = self.image.getHeight()
+  self.width = self.image:getWidth()
+  self.height = self.image:getHeight()
   
   self.last = {}
   self.last.x = self.x
