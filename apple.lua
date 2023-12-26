aecb2d5a0df127a4b0508156cf173fff1323a395
Guysus <137240103+Guysus@users.noreply.github.com>
2023-12-26 09:48:14 -0800

    Update apple.lua

--- apple.lua
+++ apple.lua
@@ -2,7 +2,20 @@
 Apple = Entity:extend()
 
 function Apple:new(x, y)
-    Apple.super.new(self, x, y, "Images/apple.png")
+    
+    apples = {}
+    
+    for i=1 do
+        table.insert(apples,
+            {
+                x = math.random(50, 650),
+                y = math.random(50, 450),
+                image = love.graphics.newImage("Images/apple.png")
+            }
+        )
+    end
+  
+    --Apple.super.new(self, x, y, "Images/apple.png")
 end
 
 function Apple:update(dt)
